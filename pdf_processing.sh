e02f4760a01482f5004736c9d906225ceeffb432
Kade Greenburg <kadegreenburg@gmail.com>
2021-08-11 14:31:41 -0600

    improved command line argument parsing

--- pdf_processing.sh
+++ pdf_processing.sh
@@ -2,15 +2,23 @@
 
 # converts pdf2json, generates a human-readable form of that,
 # then converts the pdf to an image with the specified resolution
-
-# how to take in optional arguments where no argument is required?
-while getopts f:r:p: opt; do
-	case "$opt" in
-		f) file=${OPTARG};;
-		r) image_res=${OPTARG};;
-		p) pretty_print=${OPTARG};;
+file=""
+image_res=150
+pretty_print=0
+while [ "$1" != "" ]; do
+	case $1 in
+	-f | --file )			shift
+							file="$1"
+							;;
+	-r | --resolution )		shift
+							image_res="$1"
+							;;
+	-p | --prettyprint )	pretty_print=1
+							;;
 	esac
+	shift
 done
+
 
 if [[ ! -f $file ]]
 then
@@ -18,8 +26,8 @@ then
 	exit
 fi
 
-json_file="${file::-4}.json"
 
+json_file="${file::-4}.json"
 # convert PDF to JSON if the JSON doesn't already exist
 if [[ ! -f "${file::-4}.json" ]]
 then
@@ -29,8 +37,9 @@ else
 	echo "${file::-4}.json already exists!"
 fi
 
+
 # convert JSON to human readable form if not already done (if flag -p = true)
-if [[ $pretty_print == true ]]
+if [[ $pretty_print == 1 ]]
 	then
 		if [[ ! -f "${file::-4}PrettyPrinted.json" ]]
 			then
